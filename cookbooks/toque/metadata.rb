d8b677b73ad5d62074981dc23fa6eb8cbedb3424
Spike Grobstein <spikegrobstein@mac.com>
2012-04-21 13:53:50 -0400

    added my name and the correct licence to the toque metadata.rb file

--- cookbooks/toque/metadata.rb
+++ cookbooks/toque/metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures toque"
+maintainer       "Spike Grobstein"
+maintainer_email "spikegrobstein@mac.com"
+license          "MIT"
+description      "Recipes for your Capistrano deployment"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
