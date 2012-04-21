10758d98e31153a53e21369dce6244ca1f68e3a6
Spike Grobstein <spikegrobstein@mac.com>
2012-04-21 13:54:06 -0400

    rewrote the user recipe to actually work properly

--- cookbooks/toque/recipes/user.rb
+++ cookbooks/toque/recipes/user.rb
@@ -2,32 +2,55 @@
 # Cookbook Name:: toque
 # Recipe:: user
 #
-# Copyright 2012, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
+# Copyright 2012, Spike Grobstein
 #
 
+# supports the following variables intitialized from capistrano:
+# * deploy_user -- the user to deploy
+# * deploy_user_shell -- the shell for the deploy user
+# * deploy_user_home -- the path to the home directory for the deploy user
+# * deploy_user_comment -- the GECOS comment field for the deploy user
+# * authorized_keys -- an array of public keys to add to the user's authorized_keys2 file
 
-deploy_user = node[:deploy_user]
+# TODO: yet to implement:
+# uid
+# gid
+# github private deploy key - key for deploying the application with ssh config file
+# groups for this user to belong to
+# base template configuration for ssh config
+
 
 user deploy_user do
   action :create
-  shell '/bin/bash'
-  home "/home/#{ deploy_user }"
-  comment "deploy user for #{ node.cap[:application] }"
-  supports :manage_home => true
+  shell     node.deploy_user_shell    || '/bin/bash'
+  home      node.deploy_user_home     || "/home/#{ node.deploy_user }"
+  comment   node.deploy_user_comment  || "deploy user for #{ node.application }"
+  supports  :manage_home => true
 end
 
-directory "/home/#{ deploy_user }/.ssh" do
-  owner deploy_user
-  group deploy_user
-  mode 0700
-end
+# configure the deploy key if there's a deploy key configured
+if node.attribute?(:authorized_keys)
+  
+  # create the user's .ssh directory
+  directory "/home/#{ deploy_user }/.ssh" do
+    owner node.deploy_user
+    group node.deploy_user
+    mode 0744
+  end
+  
+  # build the content of the authorized_keys2 file
+  keys = [*node.authorized_keys].join "\n"
 
-# now add keys
-cookbook_file "/home/#{ deploy_user }/.ssh/authorized_keys2" do
-  source node[:deploy_key]
-  mode "0600"
-  owner deploy_user
-  group deploy_user
+  # now add keys
+  file "/home/#{ deploy_user }/.ssh/authorized_keys2" do
+    content keys
+    backup :false
+    mode "0600"
+    owner deploy_user
+    group deploy_user
+    
+    # will always overwrite the file. this way, you can easily manage this file
+    action :create
+  end
+  
 end
