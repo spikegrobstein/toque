eca875d0a56f0d2ea1f00ad46641f13dd92e881c
Spike Grobstein <spike@ticketevolution.com>
2012-11-19 15:38:16 -0500

    ignore vars that start with mailgun.

--- lib/toque.rb
+++ lib/toque.rb
@@ -2,23 +2,23 @@ require "toque/version"
 require 'json'
 
 module Toque
-  
+
   class << self
-    
+
     attr_reader :chef_recipes
-    
+
     # Register a recipe to be run
     # recipe names should be namespaced (eg: toque::database)
     # options will be passed to the capistrano run() function
     def chef_recipe(recipe_name, options={})
       @chef_recipes ||= {}
-      
+
       # if recipe_name is a symbol
       # it's implicitely a toque recipe, so prefix that shit
       if recipe_name.class == Symbol
         recipe_name = "toque::#{recipe_name}"
       end
-      
+
       @chef_recipes[recipe_name] = options
     end
 
@@ -32,28 +32,29 @@ module Toque
         :logger,
         :password
       ]
-  
+
       # build the json data
       json_data = {}
       variables.each do |k, v|
         begin
           # ignore ignored vars
           next if ignored_vars.include?(k.to_sym)
-          
+          next if k.match(/^mailgun/) # internal fix for our app
+
           # if the variable is callable, call it, so as to dereference it
           v = v.call if v.respond_to? :call
-    
+
           json_data[k] = v
         rescue
           # do nothing.
         end
       end
-      
+
       # set the run_list var unless it's nil
       unless run_list.nil?
         json_data[:run_list] = run_list
       end
-  
+
       json_data
     end
 
